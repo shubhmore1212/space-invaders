44620b02c6bf7a0d09fcf99a05c66b7a40885a25
shubham <shubham.more85@gmail.com>
2024-01-27 20:02:33 +0530

    Add multiple enemy and timer to game

--- sketch.js
+++ sketch.js
@@ -15,10 +15,23 @@ var rSpeed = 5;
 var fire = false;
 
 //aliens
-var a1X = 50;
-var a1Y = 150;
 var aWidth = 40;
 var aHeight = 40;
+
+var a1X = 50;
+var a1Y = 150;
+
+var a2X = 110;
+var a2Y = 150;
+
+var a3X = 170;
+var a3Y = 150;
+
+var a4X = 230;
+var a4Y = 150;
+
+var a5X = 290;
+var a5Y = 150;
 
 //score-pointer
 var score = 0;
@@ -28,6 +41,11 @@ var stage = 0;
 //stage 0 = splash
 //stage 1 = game
 //stage 2 = win
+
+var totalTime;
+var splashTime;
+var gameTime;
+var timeLimit = 10;
 
 //multimedia;
 var playerImage;
@@ -42,11 +60,14 @@ function setup() {
   rectMode(CENTER);
   textAlign(CENTER);
   imageMode(CENTER);
-
+  enemy();
   backgroundMusic.play();
 }
 
 function draw() {
+  //start clock
+  totalTime = millis();
+
   if (stage == 0) {
     splash();
   }
@@ -61,10 +82,38 @@ function draw() {
     fireSound = null;
   }
 
-  if (mouseIsPressed) {
+  if (stage == 3) {
+    lose();
+  }
+}
+
+function mousePressed() {
+  if (stage == 0) {
     stage = 1;
     startSound.play();
   }
+}
+
+function lose() {
+  background(255, 0, 0);
+
+  //outer border
+  stroke(255, 0, 0);
+  noFill();
+  strokeWeight(3);
+  rect(width / 2, height / 2, width, height);
+  noStroke();
+
+  //words for splash
+  fill(0);
+  textSize(80);
+  textFont(titleFont);
+  text("YOU LOSE", width / 2, 100);
+  text("BUDDY!", width / 2, 180);
+
+  textSize(40);
+  textFont(bodyFont);
+  text("REFRESH TO PLAY AGAIN", width / 2, 300);
 }
 
 function win() {
@@ -86,10 +135,14 @@ function win() {
 
   textSize(40);
   textFont(bodyFont);
-  text("REFRESH TO PLAY AGAIN", width / 2, 300);
+  text("YOUR GAMETIME WAS", width / 2, 300);
+  text(gameTime + " seconds", width / 2, 340);
+  text("REFRESH TO PLAY AGAIN", width / 2, 450);
 }
 
 function splash() {
+  //start clock
+  splashTime = totalTime;
   background(0);
 
   //outer border
@@ -139,11 +192,6 @@ function game() {
   fill(0, 255, 0);
   rect(width / 2, 25, width, 50);
 
-  //aliens
-  noStroke();
-  fill(255);
-  image(enemyImage, a1X, a1Y, aWidth, aHeight);
-
   //player
   noStroke();
   fill(0, 0, 255);
@@ -152,22 +200,36 @@ function game() {
   //rockets
   rockets();
 
+  //stop splash time and start game time
+  splashTime = splashTime; //stop splash time to save amount of time we are on splash
+  gameTime = Math.ceil((totalTime - splashTime) / 1000);
+
+  //aliens
+  noStroke();
+  fill(255);
+  for (let i = 0; i < enemyList.length; i++) {
+    image(enemyList[i].image, enemyList[i].xpos, enemyList[i].ypos, 30, 30);
+  }
+
   //collision betn rockets and aliens
-  if (
-    r1X >= a1X - aWidth / 2 &&
-    r1X <= a1X + aWidth / 2 &&
-    r1Y >= a1Y - aHeight / 2 &&
-    r1Y <= a1Y + aHeight / 2
-  ) {
-    //remove alien
-    a1X = -1000;
-    explosionSound.play();
-    r1Position = 2;
-    //add point
-    score = score + 1;
+  for (let i = 0; i < enemyList.length; i++) {
+    if (
+      r1X >= enemyList[i].xpos - aWidth / 2 &&
+      r1X <= enemyList[i].xpos + aWidth / 2 &&
+      r1Y >= enemyList[i].ypos - aHeight / 2 &&
+      r1Y <= enemyList[i].ypos + aHeight / 2
+    ) {
+      //remove alien
+      enemyList[i].xpos = -1000;
+      explosionSound.play();
+      r1Position = 2;
+      //add point
+      score = score + 1;
+    }
   }
 
   // score-board
+  //score
   fill(0);
   textSize(25);
   textFont(titleFont);
@@ -176,11 +238,43 @@ function game() {
   textSize(35);
   text(score, score.length > 3 ? 100 : 110, 35);
 
+  //timer
+  fill(0);
+  textSize(25);
+  textFont(titleFont);
+  text("Time:", 510, 35);
+  textFont(bodyFont);
+  textSize(35);
+  text(timeLimit - gameTime, 570, 35);
+
   //exiting stages
-  if (score >= 1) {
+  if (score >= 3) {
+    gameTime = gameTime; //stop game timer
     stage = 2;
     winSound.play();
   }
+
+  if (gameTime >= timeLimit) {
+    stage = 3;
+    gameOverSound.play();
+  }
+}
+
+let enemyList = [];
+function enemy() {
+  for (let i = 0; i < 9; i++) {
+    let enemy = new Enemy();
+    enemy.xpos = a1X + i * 60;
+    enemyList.push(enemy);
+  }
+
+  for (let i = 0; i < 9; i++) {
+    let enemy = new Enemy();
+    enemy.xpos = a1X + i * 60;
+    enemy.ypos = a1Y + 60;
+    enemyList.push(enemy);
+  }
+  console.log(enemyList);
 }
 
 function rockets() {
@@ -230,7 +324,7 @@ function keyPressed() {
 
 //only for alphanumeric types
 function keyTyped() {
-  if (key == "s" && keyIsPressed) {
+  if ((key == "s" || key == "S") && keyIsPressed) {
     fire = true;
     fireSound.play();
   } else {
@@ -253,4 +347,12 @@ function preload() {
   gameOverSound = loadSound("audio/gameOver.mp3");
   startSound = loadSound("audio/start.mp3");
   winSound = loadSound("audio/bonus.mp3");
+}
+
+class Enemy {
+  constructor() {
+    this.xpos = a1X;
+    this.ypos = a1Y;
+    this.image = enemyImage;
+  }
 }
